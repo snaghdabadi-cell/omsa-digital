fc5ce39768ff337e6d598bf77df425f67af90dcb
Sajran <snaghdabadi@gmail.com>
2026-08-16 13:33:46 +0400

    Add OMSA favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -93,6 +93,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "format-detection", content: "telephone=no" },
     ],
     links: [
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
